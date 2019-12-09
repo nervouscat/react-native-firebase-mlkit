969f24bae4e6a25de36ad68047d68f9f9af7c6ea
Tarek Moubarak <tarek.moubarak@gmail.com>
2019-12-09 17:23:31 +0200

    Update RNMlKit.m

--- ios/RNMlKit.m
+++ ios/RNMlKit.m
@@ -2,8 +2,8 @@
 
 #import <React/RCTBridge.h>
 
-#import <FirebaseCore/FirebaseCore.h>
-#import <FirebaseMLVision/FirebaseMLVision.h>
+//#import <FirebaseCore/FirebaseCore.h>
+//#import <FirebaseMLVision/FirebaseMLVision.h>
 #import <React/RCTImageLoader.h>
 
 @implementation RNMlKit
@@ -28,186 +28,189 @@ RCT_REMAP_METHOD(deviceTextRecognition, deviceTextRecognition:(NSString *)imageP
         return;
     }
     
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        FIRVision *vision = [FIRVision vision];
-        FIRVisionTextRecognizer *textRecognizer = [vision onDeviceTextRecognizer];
+    NSMutableArray *output = [NSMutableArray array];
+    resolve(output);
+    
+//     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//         FIRVision *vision = [FIRVision vision];
+//         FIRVisionTextRecognizer *textRecognizer = [vision onDeviceTextRecognizer];
         
-        //NSDictionary *d = [[NSDictionary alloc] init];
-        //if Base64
-        UIImage *image = [self decodeBase64ToImage:imagePath];
-        if (!image) {
-            //If URI
-            image = [[UIImage alloc] initWithContentsOfFile:imagePath];
-        }
-        //UIImage *image = [[UIImage alloc] initWithContentsOfFile:imagePath];
-        //NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:imagePath]];
-        //UIImage *image = [UIImage imageWithData:imageData];
+//         //NSDictionary *d = [[NSDictionary alloc] init];
+//         //if Base64
+//         UIImage *image = [self decodeBase64ToImage:imagePath];
+//         if (!image) {
+//             //If URI
+//             image = [[UIImage alloc] initWithContentsOfFile:imagePath];
+//         }
+//         //UIImage *image = [[UIImage alloc] initWithContentsOfFile:imagePath];
+//         //NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:imagePath]];
+//         //UIImage *image = [UIImage imageWithData:imageData];
         
-        if (!image) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                resolve(@NO);
-            });
-            return;
-        }
+//         if (!image) {
+//             dispatch_async(dispatch_get_main_queue(), ^{
+//                 resolve(@NO);
+//             });
+//             return;
+//         }
         
-        FIRVisionImage *handler = [[FIRVisionImage alloc] initWithImage:image];
+//         FIRVisionImage *handler = [[FIRVisionImage alloc] initWithImage:image];
         
-        [textRecognizer processImage:handler completion:^(FIRVisionText *_Nullable result, NSError *_Nullable error) {
-            if (error != nil || result == nil) {
-                NSString *errorString = error ? error.localizedDescription : detectionNoResultsMessage;
-                NSDictionary *pData = @{
-                                        @"error": [NSMutableString stringWithFormat:@"On-Device text detection failed with error: %@", errorString],
-                                        };
-                // Running on background thread, don't call UIKit
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    resolve(pData);
-                });
-                return;
-            }
+//         [textRecognizer processImage:handler completion:^(FIRVisionText *_Nullable result, NSError *_Nullable error) {
+//             if (error != nil || result == nil) {
+//                 NSString *errorString = error ? error.localizedDescription : detectionNoResultsMessage;
+//                 NSDictionary *pData = @{
+//                                         @"error": [NSMutableString stringWithFormat:@"On-Device text detection failed with error: %@", errorString],
+//                                         };
+//                 // Running on background thread, don't call UIKit
+//                 dispatch_async(dispatch_get_main_queue(), ^{
+//                     resolve(pData);
+//                 });
+//                 return;
+//             }
             
-            //  CGRect boundingBox;
-            //  CGSize size;
-            //CGPoint origin;
-            NSMutableArray *output = [NSMutableArray array];
+//             //  CGRect boundingBox;
+//             //  CGSize size;
+//             //CGPoint origin;
+//             NSMutableArray *output = [NSMutableArray array];
             
-            for (FIRVisionTextBlock *block in result.blocks) {
-                NSMutableDictionary *blockDict = [NSMutableDictionary dictionary];
-                NSMutableDictionary *bounding = [NSMutableDictionary dictionary];
+//             for (FIRVisionTextBlock *block in result.blocks) {
+//                 NSMutableDictionary *blockDict = [NSMutableDictionary dictionary];
+//                 NSMutableDictionary *bounding = [NSMutableDictionary dictionary];
                 
-                NSMutableDictionary *origin = [NSMutableDictionary dictionary];
-                NSMutableDictionary *originRight = [NSMutableDictionary dictionary];
-                NSMutableDictionary *center = [NSMutableDictionary dictionary];
+//                 NSMutableDictionary *origin = [NSMutableDictionary dictionary];
+//                 NSMutableDictionary *originRight = [NSMutableDictionary dictionary];
+//                 NSMutableDictionary *center = [NSMutableDictionary dictionary];
                 
-                NSMutableDictionary *size = [NSMutableDictionary dictionary];
-                blockDict[@"blockText"] = block.text;
+//                 NSMutableDictionary *size = [NSMutableDictionary dictionary];
+//                 blockDict[@"blockText"] = block.text;
                 
-                NSMutableArray *langsArray = [NSMutableArray array];
-                for (FIRVisionTextRecognizedLanguage *lang in block.recognizedLanguages) {
-                    if(lang.languageCode)
-                        [langsArray addObject:lang.languageCode];
-                }
-                blockDict[@"recognized_lang"] = langsArray;
-                
-                
-                
-                // NSArray<NSValue *> *blockCornerPoints = block.cornerPoints;
-                CGRect blockFrame = block.frame;
-                origin[@"x"] = @(blockFrame.origin.x);
-                origin[@"y"] = @(blockFrame.origin.y);
-                
-                originRight[@"x"] = @(blockFrame.origin.x+blockFrame.size.width);
-                originRight[@"y"] = @(blockFrame.origin.y);
-                center[@"x"] = @(blockFrame.origin.x +(blockFrame.size.width/2));
-                center[@"y"] = @(blockFrame.origin.y+ (blockFrame.size.height/2));
-                size[@"width"]= @(blockFrame.size.width);
-                size[@"height"]= @(blockFrame.size.height);
+//                 NSMutableArray *langsArray = [NSMutableArray array];
+//                 for (FIRVisionTextRecognizedLanguage *lang in block.recognizedLanguages) {
+//                     if(lang.languageCode)
+//                         [langsArray addObject:lang.languageCode];
+//                 }
+//                 blockDict[@"recognized_lang"] = langsArray;
                 
                 
-                bounding[@"origin"] = origin;
-                bounding[@"origin_right"] = originRight;
-                bounding[@"center"] = center;
-                bounding[@"size"] = size;
                 
-                //NSMutableDictionary *blockDict2 = block.;
+//                 // NSArray<NSValue *> *blockCornerPoints = block.cornerPoints;
+//                 CGRect blockFrame = block.frame;
+//                 origin[@"x"] = @(blockFrame.origin.x);
+//                 origin[@"y"] = @(blockFrame.origin.y);
                 
-                blockDict[@"bounding"] = bounding;
-                //[output addObject:blocks];
-                NSMutableArray *linesArray = [NSMutableArray array];
-                for (FIRVisionTextLine *line in block.lines) {
-                    NSMutableDictionary *lineDict = [NSMutableDictionary dictionary];
+//                 originRight[@"x"] = @(blockFrame.origin.x+blockFrame.size.width);
+//                 originRight[@"y"] = @(blockFrame.origin.y);
+//                 center[@"x"] = @(blockFrame.origin.x +(blockFrame.size.width/2));
+//                 center[@"y"] = @(blockFrame.origin.y+ (blockFrame.size.height/2));
+//                 size[@"width"]= @(blockFrame.size.width);
+//                 size[@"height"]= @(blockFrame.size.height);
+                
+                
+//                 bounding[@"origin"] = origin;
+//                 bounding[@"origin_right"] = originRight;
+//                 bounding[@"center"] = center;
+//                 bounding[@"size"] = size;
+                
+//                 //NSMutableDictionary *blockDict2 = block.;
+                
+//                 blockDict[@"bounding"] = bounding;
+//                 //[output addObject:blocks];
+//                 NSMutableArray *linesArray = [NSMutableArray array];
+//                 for (FIRVisionTextLine *line in block.lines) {
+//                     NSMutableDictionary *lineDict = [NSMutableDictionary dictionary];
                     
-                    NSMutableDictionary *bounding = [NSMutableDictionary dictionary];
-                    NSMutableDictionary *origin = [NSMutableDictionary dictionary];
-                    NSMutableDictionary *originRight = [NSMutableDictionary dictionary];
-                    NSMutableDictionary *center = [NSMutableDictionary dictionary];
+//                     NSMutableDictionary *bounding = [NSMutableDictionary dictionary];
+//                     NSMutableDictionary *origin = [NSMutableDictionary dictionary];
+//                     NSMutableDictionary *originRight = [NSMutableDictionary dictionary];
+//                     NSMutableDictionary *center = [NSMutableDictionary dictionary];
                     
-                    NSMutableDictionary *size = [NSMutableDictionary dictionary];
+//                     NSMutableDictionary *size = [NSMutableDictionary dictionary];
                     
-                    lineDict[@"lineText"] = line.text;
+//                     lineDict[@"lineText"] = line.text;
                     
-                    NSMutableArray *langsArray = [NSMutableArray array];
-                    for (FIRVisionTextRecognizedLanguage *lang in line.recognizedLanguages) {
-                        if(lang.languageCode)
-                            [langsArray addObject:lang.languageCode];
-                    }
-                    lineDict[@"recognized_lang"] = langsArray;
+//                     NSMutableArray *langsArray = [NSMutableArray array];
+//                     for (FIRVisionTextRecognizedLanguage *lang in line.recognizedLanguages) {
+//                         if(lang.languageCode)
+//                             [langsArray addObject:lang.languageCode];
+//                     }
+//                     lineDict[@"recognized_lang"] = langsArray;
                     
-                    CGRect lineFrame = line.frame;
-                    origin[@"x"] = @(lineFrame.origin.x);
-                    origin[@"y"] = @(lineFrame.origin.y);
-                    originRight[@"x"] = @(blockFrame.origin.x+blockFrame.size.width);
-                    originRight[@"y"] = @(blockFrame.origin.y);
-                    center[@"x"] = @(blockFrame.origin.x +(blockFrame.size.width/2));
-                    center[@"y"] = @(blockFrame.origin.y+ (blockFrame.size.height/2));
-                    size[@"width"]= @(lineFrame.size.width);
-                    size[@"height"]= @(lineFrame.size.height);
+//                     CGRect lineFrame = line.frame;
+//                     origin[@"x"] = @(lineFrame.origin.x);
+//                     origin[@"y"] = @(lineFrame.origin.y);
+//                     originRight[@"x"] = @(blockFrame.origin.x+blockFrame.size.width);
+//                     originRight[@"y"] = @(blockFrame.origin.y);
+//                     center[@"x"] = @(blockFrame.origin.x +(blockFrame.size.width/2));
+//                     center[@"y"] = @(blockFrame.origin.y+ (blockFrame.size.height/2));
+//                     size[@"width"]= @(lineFrame.size.width);
+//                     size[@"height"]= @(lineFrame.size.height);
                     
                     
-                    bounding[@"origin"] = origin;
-                    bounding[@"origin_right"] = originRight;
-                    bounding[@"center"] = center;
-                    bounding[@"size"] = size;
+//                     bounding[@"origin"] = origin;
+//                     bounding[@"origin_right"] = originRight;
+//                     bounding[@"center"] = center;
+//                     bounding[@"size"] = size;
                     
-                    //NSMutableDictionary *blockDict2 = block.;
+//                     //NSMutableDictionary *blockDict2 = block.;
                     
-                    lineDict[@"bounding"] = bounding;
+//                     lineDict[@"bounding"] = bounding;
                     
-                    ///[output addObject:lines];
-                    NSMutableArray *elementsArray = [NSMutableArray array];
-                    for (FIRVisionTextElement *element in line.elements) {
-                        NSMutableDictionary *elementDict = [NSMutableDictionary dictionary];
+//                     ///[output addObject:lines];
+//                     NSMutableArray *elementsArray = [NSMutableArray array];
+//                     for (FIRVisionTextElement *element in line.elements) {
+//                         NSMutableDictionary *elementDict = [NSMutableDictionary dictionary];
                         
-                        NSMutableDictionary *bounding = [NSMutableDictionary dictionary];
-                        NSMutableDictionary *origin = [NSMutableDictionary dictionary];
-                        NSMutableDictionary *originRight = [NSMutableDictionary dictionary];
-                        NSMutableDictionary *center = [NSMutableDictionary dictionary];
-                        NSMutableDictionary *size = [NSMutableDictionary dictionary];
-                        
-                        
-                        elementDict[@"elementText"] = element.text;
-                        
-                        NSMutableArray *langsArray = [NSMutableArray array];
-                        for (FIRVisionTextRecognizedLanguage *lang in element.recognizedLanguages) {
-                            if(lang.languageCode)
-                                [langsArray addObject:lang.languageCode];
-                        }
-                        elementDict[@"recognized_lang"] = langsArray;
-                        
-                        CGRect elementFrame = element.frame;
-                        origin[@"x"] = @(elementFrame.origin.x);
-                        origin[@"y"] = @(elementFrame.origin.y);
-                        originRight[@"x"] = @(blockFrame.origin.x+blockFrame.size.width);
-                        originRight[@"y"] = @(blockFrame.origin.y);
-                        center[@"x"] = @(blockFrame.origin.x +(blockFrame.size.width/2));
-                        center[@"y"] = @(blockFrame.origin.y+ (blockFrame.size.height/2));
-                        size[@"width"]= @(elementFrame.size.width);
-                        size[@"height"]= @(elementFrame.size.height);
+//                         NSMutableDictionary *bounding = [NSMutableDictionary dictionary];
+//                         NSMutableDictionary *origin = [NSMutableDictionary dictionary];
+//                         NSMutableDictionary *originRight = [NSMutableDictionary dictionary];
+//                         NSMutableDictionary *center = [NSMutableDictionary dictionary];
+//                         NSMutableDictionary *size = [NSMutableDictionary dictionary];
                         
                         
-                        bounding[@"origin"] = origin;
-                        bounding[@"origin_right"] = originRight;
-                        bounding[@"center"] = center;
-                        bounding[@"size"] = size;
+//                         elementDict[@"elementText"] = element.text;
                         
-                        //NSMutableDictionary *blockDict2 = block.;
+//                         NSMutableArray *langsArray = [NSMutableArray array];
+//                         for (FIRVisionTextRecognizedLanguage *lang in element.recognizedLanguages) {
+//                             if(lang.languageCode)
+//                                 [langsArray addObject:lang.languageCode];
+//                         }
+//                         elementDict[@"recognized_lang"] = langsArray;
                         
-                        elementDict[@"bounding"] = bounding;
+//                         CGRect elementFrame = element.frame;
+//                         origin[@"x"] = @(elementFrame.origin.x);
+//                         origin[@"y"] = @(elementFrame.origin.y);
+//                         originRight[@"x"] = @(blockFrame.origin.x+blockFrame.size.width);
+//                         originRight[@"y"] = @(blockFrame.origin.y);
+//                         center[@"x"] = @(blockFrame.origin.x +(blockFrame.size.width/2));
+//                         center[@"y"] = @(blockFrame.origin.y+ (blockFrame.size.height/2));
+//                         size[@"width"]= @(elementFrame.size.width);
+//                         size[@"height"]= @(elementFrame.size.height);
                         
-                        [elementsArray addObject:elementDict];
                         
-                    }
-                    lineDict[@"elements"] = elementsArray;
-                    [output addObject:lineDict];
-                }
-               // blockDict[@"lines"] = linesArray;
-               // [output addObject:blockDict];
-            }
+//                         bounding[@"origin"] = origin;
+//                         bounding[@"origin_right"] = originRight;
+//                         bounding[@"center"] = center;
+//                         bounding[@"size"] = size;
+                        
+//                         //NSMutableDictionary *blockDict2 = block.;
+                        
+//                         elementDict[@"bounding"] = bounding;
+                        
+//                         [elementsArray addObject:elementDict];
+                        
+//                     }
+//                     lineDict[@"elements"] = elementsArray;
+//                     [output addObject:lineDict];
+//                 }
+//                // blockDict[@"lines"] = linesArray;
+//                // [output addObject:blockDict];
+//             }
             
-            dispatch_async(dispatch_get_main_queue(), ^{
-                resolve(output);
-            });
-        }];
-    });
+//             dispatch_async(dispatch_get_main_queue(), ^{
+//                 resolve(output);
+//             });
+//         }];
+//     });
 }
 @end
 
